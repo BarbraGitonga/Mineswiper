a2673dc9060ce83715c92ccbf2b2dde434a9c18f
Barbra Gitonga <gitongabarbra@gmail.com>
2024-02-28 15:53:54 +0300

    added utils file

--- Mine/main.py
+++ Mine/main.py
@@ -1,5 +1,8 @@
+from logging import RootLogger
+from pickle import FRAME
 from tkinter import *
 import settings as st
+import utils
 
 root = Tk()
 root.configure(bg="grey")
@@ -10,19 +13,25 @@ root.resizable(False, False) #disables the maximize button on window
 top_frame = Frame(
     root,
     bg='black',
-    width= 720,
-    height=45,
+    width= utils.width_prct(100),
+    height=utils.height_prct(12.5),
 )
-
 top_frame.place(x=0, y=0)
 
 left_frame = Frame(
     root,
     bg = 'white',
-    width=100,
-    height=315,
+    width=utils.width_prct(15),
+    height=utils.height_prct(100),
 )
 left_frame.place(x=0, y=45)
 
+center_frame = Frame(
+    root,
+    bg='grey',
+    width = utils.width_prct(85),
+    height = utils.height_prct(86.5)
+)
+center_frame.place(x=utils.width_prct(15), y=utils.height_prct(12.5))
 #Run the window
 root.mainloop() # opens window
---
+++ Mine/utils.py
@@ -0,0 +1,7 @@
+import settings as st
+
+def height_prct(percentage):
+    return (st.HEIGHT / 100) * percentage
+
+def width_prct(percentage):
+    return (st.WIDTH / 100) * percentage
